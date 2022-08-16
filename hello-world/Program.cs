56b5ae28d84754ec2f8648699dd28f936b7a814e
Jarret Nachtigal <jarretn1224@gmail.com>
2022-08-16 13:34:07 -0400

    initial commit, hello world

---
+++ hello-world/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // print
+      Console.WriteLine("What is your name?");
+      // read line from cmd prompt
+      var name = Console.ReadLine();
+      // current date MM/DD/YYYY
+      var currentDate = DateTime.Now;
+      // new line - like \n, string interpolation
+      Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
+      // print with no \n
+      Console.Write($"{Environment.NewLine}Press any key to exit...");
+      // read keyboard input - how? - doesn't wait for enter key
+      Console.ReadKey(true);
+    }
+  }
+}

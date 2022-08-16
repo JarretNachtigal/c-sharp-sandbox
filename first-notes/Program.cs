f89f2ea4241fadb4e4070d3656d5779a28cc5b9c
Jarret Nachtigal <jarretn1224@gmail.com>
2022-08-16 16:07:25 -0400

    tuples

--- first-notes/Program.cs
+++ first-notes/Program.cs
@@ -168,3 +168,10 @@ namespace TestingEnum
     }
   }
 }
+
+// tuples
+
+// (double Sum, int Count) t2 = (4.5, 3);
+// Console.WriteLine($"Sum of {t2.Count} elements is {t2.Sum}.");
+//Output:
+//Sum of 3 elements is 4.5.

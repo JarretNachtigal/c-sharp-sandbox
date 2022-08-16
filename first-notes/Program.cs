bd31f4169de0b5bcd88c8323a94a7770b8144a4e
Jarret Nachtigal <jarretn1224@gmail.com>
2022-08-16 13:58:20 -0400

    notes on reference vs value types

--- first-notes/Program.cs
+++ first-notes/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// make new folder mkdir ...
+// new program dotnet new console --framework net6.0 -n name
+// dotnet run (optional filename)
 Console.WriteLine("Hello, World!");
+
+
+// reference vs value types
+
+// value/primitive - store value directly in memory. if passed into a method and then changed inside of it, 
+//a copy is made instead, the original remains the same
+
+// bool
+// byte
+// char
+// decimal
+// double
+// enum
+// float
+// int
+// long
+// sbyte
+// short
+// struct
+// uint
+// ulong
+// ushort
+
+// reference - stores data as a reference/ stores the a pointer to the address of the data - if passed into a method and changed
+// the same location is referenced, and it will change

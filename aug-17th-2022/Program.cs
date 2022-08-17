7d603cb5dce7dec563112902af55b1bd80df3903
Jarret Nachtigal <jarretn1224@gmail.com>
2022-08-17 14:15:27 -0400

    method notes

--- aug-17th-2022/Program.cs
+++ aug-17th-2022/Program.cs
@@ -4,6 +4,17 @@ using System;
 
 class StaticAndInstanceFieldsExample
 {
+  //An instance constructor is a member that implements the actions required to initialize an instance of a class. 
+  //A static constructor is a member that implements the actions required to initialize a class itself when it's first loaded.
+  static StaticAndInstanceFieldsExample()
+  {
+    shared = 1000;
+  }
+  public StaticAndInstanceFieldsExample()
+  {
+
+  }
+
   // static fields will be shared between all instances of the class
   static int shared;
   int instance;
@@ -29,6 +40,7 @@ class StaticAndInstanceFieldsExample
   }
 }
 
+// overriding, abstract methods and classes, overloading all work like java
 
 class Program
 {
@@ -38,12 +50,11 @@ class Program
     StaticAndInstanceFieldsExample test2 = new StaticAndInstanceFieldsExample();
     test1.setInstance(1);
     test2.setInstance(2);
-    StaticAndInstanceFieldsExample.setShared(100); // called this way because its a static method
     // test1.setShared(10); // would be called this way if not a static method
-    Console.WriteLine($"test1 = {test1.getInstance()}, test2 = {test2.getInstance()} shared value = {test1.getShared()} and {test2.getShared()}");
+    Console.WriteLine($"test1 = {test1.getInstance()}, test2 = {test2.getInstance()} shared value from static constructor = {test1.getShared()} and {test2.getShared()}");
     Console.WriteLine("changing values");
+    StaticAndInstanceFieldsExample.setShared(100); // called this way because its a static method
     test1.setInstance(10);
-    StaticAndInstanceFieldsExample.setShared(200);
     Console.WriteLine($"test1 = {test1.getInstance()}, test2 = {test2.getInstance()} shared value = {test1.getShared()} and {test2.getShared()}");
   }
 

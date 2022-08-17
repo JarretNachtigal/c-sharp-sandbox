4afce28ed646c8ad80d79dfd8086c14c6add0d82
Jarret Nachtigal <jarretn1224@gmail.com>
2022-08-17 13:29:42 -0400

    method notes

--- aug-17th-2022/Program.cs
+++ aug-17th-2022/Program.cs
@@ -9,9 +9,10 @@ class Program
 
   }
 
-  // remembet that String is a reference type, not a value type
+  // remember that String is a reference type, not a value type
   public static String exampleFunction(int num)
   {
+    // initialized on the stack, won't persist
     int localVariable = 4;
     Console.WriteLine($"{localVariable}");
     return $"{num} entered";

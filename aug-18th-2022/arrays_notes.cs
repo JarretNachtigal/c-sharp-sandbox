48457b6ce9dc3a7df7478e78c0021f37ac78657c
Jarret Nachtigal <jarretn1224@gmail.com>
2022-08-18 16:32:46 -0400

    adds arrays_notes and ArraysNotes namespace, reorganize

---
+++ aug-18th-2022/arrays_notes.cs
@@ -0,0 +1,65 @@
+namespace ArraysNotes
+{
+  class ArraysNotes
+  {
+    public static void Notes()
+    {
+
+      //arrays
+      // - reference type
+      // type[] name = new type[length]
+      // left side sets aside space for a refence to the array, right side instatiates array dynamically at runtime
+      int[] a = new int[10];
+      //iterate - fill
+      for (int i = 0; i < a.Length; i++)
+      {
+        a[i] = i;
+      }
+      //iterate - print
+      for (int i = 0; i < a.Length; i++)
+      {
+        // Console.WriteLine(a[i]);
+      }
+
+      //multi-dimensional array
+      int[,,] b = new int[5, 4, 3]; // 3d array of side 15 by 10, by 5
+                                    // iterate - fill
+      int count = 0;
+      for (int i = 0; i < b.GetLength(0); i++) // 5
+      {
+        for (int j = 0; j < b.GetLength(1); j++) // 4
+        {
+          for (int k = 0; k < b.GetLength(2); k++) // 3
+          {
+            b[i, j, k] = count;
+            count += 1;
+          }
+        }
+      }
+      // iterate - print
+      // "loops through a [4][3] size array 5 times"
+      for (int i = 0; i < b.GetLength(0); i++) // 5
+      {
+        // loops through a [3] size array 4 times
+        for (int j = 0; j < b.GetLength(1); j++) // 4
+        {
+          // loops prints all 3 ints in the array
+          for (int k = 0; k < b.GetLength(2); k++) // 3
+          {
+            Console.Write($"{b[i, j, k]}, ");
+          }
+        }
+        Console.WriteLine();
+      }
+
+      // easier loop
+      foreach (int item in b)
+      {
+        // Console.WriteLine(item);
+      }
+
+      // init with values
+      int[] d = new int[3] { 1, 2, 3 };
+    }
+  }
+}

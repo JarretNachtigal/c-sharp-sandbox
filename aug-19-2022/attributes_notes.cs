dd767e3642310faaf8afdab5c9ae59ec013be732
Jarret Nachtigal <jarretn1224@gmail.com>
2022-08-19 15:34:47 -0400

    notes, async_notes.cs, attribute_notes.cs

--- aug-19-2022/attributes_notes.cs
+++ aug-19-2022/attributes_notes.cs
@@ -1,21 +1,21 @@
 // "can be placed on program entities to provide links to their associated documentation."
 // if they end in the word Attribute, it can be ommited when used
 // ex" Help(...)
-public class HelpAttribute : Attribute
-{
-  string _url;
-  string _topic;
+// public class HelpAttribute : Attribute
+// {
+//   string _url;
+//   string _topic;
 
-  public HelpAttribute(string url) => _url = url;
+//   public HelpAttribute(string url) => _url = url;
 
-  public string Url => _url;
+//   public string Url => _url;
 
-  public string Topic
-  {
-    get => _topic;
-    set => _topic = value;
-  }
-}
+//   public string Topic
+//   {
+//     get => _topic;
+//     set => _topic = value;
+//   }
+// }
 
 // support modifiers - control accessability of methods
 // public

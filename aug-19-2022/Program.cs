dce8852af572d7c1643a08be325072394c73935a
Jarret Nachtigal <jarretn1224@gmail.com>
2022-08-19 15:00:39 -0400

    notes

--- aug-19-2022/Program.cs
+++ aug-19-2022/Program.cs
@@ -2,7 +2,11 @@
 {
 
   // example from c# documentation for experimentation
-  delegate double Function(double x);
+  // commenting this out breaks lines:
+  // double[] sines = Apply(a, Math.Sin); 
+  // double[] doubles = Apply(a, m.Multiply);
+  // static double[] Apply(double[] a, Function f) {...}
+  delegate double Function(double x); // a reference to a method with a double as a param
 
   class Multiplier
   {
@@ -15,20 +19,21 @@
 
   class DelegateExample
   {
-    static double[] Apply(double[] a, Function f)
+    // static function taking a delegate as a param
+    static double[] Apply(double[] a, Function f) // for each in a[], call Function f and pass in a[i]
     {
       var result = new double[a.Length];
-      for (int i = 0; i < a.Length; i++) result[i] = f(a[i]);
+      for (int i = 0; i < a.Length; i++) result[i] = f(a[i]); // delegate param called here (Math.sin or m.Multiply or (x) => x* x in this case)
       return result;
     }
 
     public static void Main(String[] args)
     {
       double[] a = { 0.0, 0.5, 1.0 }; // example data
-      double[] squares = Apply(a, (x) => x * x); // example call with lambda function param
-      double[] sines = Apply(a, Math.Sin); // example call with lambda function param
+      double[] squares = Apply(a, (x) => x * x); // example call with function param - (x) => x * x
+      double[] sines = Apply(a, Math.Sin); // example call with function param - Math.sin
       Multiplier m = new(2.0); // instantiate Miltiplier class
-      double[] doubles = Apply(a, m.Multiply); // example call with lambda function param
+      double[] doubles = Apply(a, m.Multiply); // example call with lambda function param - m.Multiply
     }
   }
 }

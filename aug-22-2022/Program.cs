56d4e81ada47d91109e6673aec19e95b4b9a5ed3
Jarret Nachtigal <jarretn1224@gmail.com>
2022-08-22 14:19:05 -0400

    type system notes

--- aug-22-2022/Program.cs
+++ aug-22-2022/Program.cs
@@ -1,7 +1,7 @@
 ﻿// - this is the order needed to use top level statements as entry point
 // - they will take priority over Main() even if Main is specified
 // - they can still access the args variable (which is never null, but can be of length 0) to access command line arguments
-// can return an int value at end like in Main()
+// can return an int value at end like in Main()/exit code
 
 // MyClass.TestMethod();
 // MyNamespace.MyClass.MyMethod();
@@ -15,3 +15,12 @@
 //     }
 
 // }
+
+
+// The information stored in a type can include the following items:
+// - The storage space that a variable of the type requires.
+// - The maximum and minimum values that it can represent.
+// - The members (methods, fields, events, and so on) that it contains.
+// - The base type it inherits from.
+// - The interface(s) it implements.
+// - The kinds of operations that are permitted.

20b1b406cb34a3f4d2dbffcf05b15e0b0fcc8aea
Jarret Nachtigal <jarretn1224@gmail.com>
2022-08-22 14:13:36 -0400

    top-level statements notes

--- aug-22-2022/Program.cs
+++ aug-22-2022/Program.cs
@@ -1,5 +1,7 @@
-﻿// this is the order needed to use top level statements as entry point
-// they will take priority over Main() even if Main is specified
+﻿// - this is the order needed to use top level statements as entry point
+// - they will take priority over Main() even if Main is specified
+// - they can still access the args variable (which is never null, but can be of length 0) to access command line arguments
+
 // MyClass.TestMethod();
 // MyNamespace.MyClass.MyMethod();
 
